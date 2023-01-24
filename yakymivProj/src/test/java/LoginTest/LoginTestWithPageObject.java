091ef8007f3a348aa16a11919e35293108a92088
IvanYakymiv <yakymiv.ivan@gmail.com>
2023-01-24 20:47:29 +0100

    login test with object

---
+++ yakymivProj/src/test/java/LoginTest/LoginTestWithPageObject.java
@@ -0,0 +1,11 @@
+package LoginTest;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class LoginTestWithPageObject extends BaseTest {
+    @Test
+    public void validLogin(){
+        loginPage.openLoginPage();
+    }
+}
