b146aacbb2ac2a57c2dc1c39d7add53975e27a91
AndriiGingin <tihonov131313@gmail.com>
2023-01-24 21:47:33 +0200

    Login Test With page Object

---
+++ ginginProj/src/main/java/org/example/LoginTestWithObject.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class LoginTestWithObject extends BaseTest {
+    @Test
+    public void validLogin(){
+        loginPage.openLoginPage();
+
+    }
+
+}
