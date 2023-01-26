758d3f9b02435d9157473cb83b3db1ec61282127
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2023-01-26 21:39:02 +0200

    CommonActionsWithElements

--- tiuliukovProj/src/main/java/pages/LoginPage.java
+++ tiuliukovProj/src/main/java/pages/LoginPage.java
@@ -1,13 +1,20 @@
 package pages;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.concurrent.ExecutionException;
 
 public class LoginPage extends ParentPage {
+    @FindBy(xpath = ".//input[@name='username' and @placeholder='Username']")
+    private WebElement inputUserName;
+    @FindBy(xpath = ".//input[@placeholder='Password']")
+    private WebElement inputPassword;
+    @FindBy(xpath = ".//button[@class='btn btn-primary btn-sm']")
+    private WebElement buttonLogin;
+
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -23,35 +30,15 @@ public class LoginPage extends ParentPage {
     }
 
     public void enterUserNameIntoInputLogin(String userName) {
-        try {
-            WebElement inputUserName =
-                    webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
-            inputUserName.clear();
-            inputUserName.sendKeys(userName);
-            logger.info("login was inputted");
-        } catch (Exception e) {
-            printErrorAndStopTest(e);
-        }
+        enterTextIntoElement(inputUserName, userName);
     }
 
     public void enterLoginNameIntoInputPassword(String password) {
-        try {
-            WebElement irputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
-            irputPassword.clear();
-            irputPassword.sendKeys(password);
-            logger.info("Password was inputted");
-        } catch (Exception e) {
-            printErrorAndStopTest(e);
-        }
+          enterTextIntoElement(inputPassword, password);
     }
 
     public void clickOnButtonLogin() {
-        try {
-            WebElement buttonLogin = webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
-            buttonLogin.click();
-            logger.info("button was clicked");
-        } catch (Exception e) {
-            printErrorAndStopTest(e);
-        }
+        clickOnElement(buttonLogin);
     }
+
 }
