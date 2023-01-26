41e44b75b959b3d3568ef0366752914081d41cbf
qasap <qa,sapaev@gmail.com>
2023-01-26 20:13:29 +0200

    test case logic

--- sapaievProj/src/main/java/pages/LoginPage.java
+++ sapaievProj/src/main/java/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends ParentPage {
 
@@ -18,6 +20,49 @@ public class LoginPage extends ParentPage {
             Assert.fail("Can not open login page" + e);
         }
     }
+
+
+    public void enterUserNameIntoInputLogin(String userName) {
+        try {
+            WebElement inputUserName =
+                    webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+            inputUserName.clear();
+            inputUserName.sendKeys(userName);
+            logger.info("login was inputted");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+
+    public void enterPasswordIntoInputPassword(String password) {
+
+        try {
+            WebElement inputPassword =
+                    webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+            inputPassword.clear();
+            inputPassword.sendKeys(password);
+            logger.info("password was entered");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+
+    public void clickOnButtonLogin() {
+        try {
+            WebElement buttonLogin =
+                    webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
+            buttonLogin.click();
+            logger.info("Button was clicked");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+
+
+
 
 
 }
--- sapaievProj/src/main/java/pages/ParentPage.java
+++ sapaievProj/src/main/java/pages/ParentPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
 public class ParentPage {
@@ -10,6 +11,12 @@ public class ParentPage {
     public ParentPage(WebDriver webDriver) {
         this.webDriver = webDriver;
 
+    }
+
+
+    protected void printErrorAndStopTest(Exception e){
+        logger.error("Can not work with element " + e);
+        Assert.fail("Can not work with element " + e);
     }
 
 
