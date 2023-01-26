f5678cd5411580822f2de9f37307e79150778419
olga-pustovoit <64926503+olga-pustovoit@users.noreply.github.com>
2023-01-26 20:13:29 +0200

    test case logic

--- pustovoitProj/src/main/java/pages/LoginPage.java
+++ pustovoitProj/src/main/java/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends ParentPage {
 
@@ -19,4 +21,38 @@ public class LoginPage extends ParentPage {
         }
     }
 
+    public void enterUserNameIntoInputLogin(String userName) {
+        try {
+            WebElement inputUserName =
+                    webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+            inputUserName.clear();
+            inputUserName.sendKeys(userName);
+            logger.info("login was inputted");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void enterPaswordIntoInputPassword(String password) {
+        try {
+            WebElement inputPassword =
+                    webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+            inputPassword.clear();
+            inputPassword.sendKeys(password);
+            logger.info("Password was entered");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void clickOnButtonLogin() {
+        try {
+            WebElement buttonLogin =
+                    webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
+            buttonLogin.click();
+            logger.info("Button was clicked");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
 }
