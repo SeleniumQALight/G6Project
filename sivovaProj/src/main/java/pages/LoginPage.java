c5ab16754053d71a32ea2e5f981a9fe935116212
IrinaSivova <jarinka.sha@gmail.com>
2023-01-26 19:12:54 +0100

    sivova - classwork 26-01 - part one commit

--- sivovaProj/src/main/java/pages/LoginPage.java
+++ sivovaProj/src/main/java/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends ParentPage {
 
@@ -20,5 +22,40 @@ public class LoginPage extends ParentPage {
             Assert.fail("Can not open Login page" + e);
         }
 
+    }
+
+    public void enterUserNameIntoInputLogin(String userName) {
+        try {
+            WebElement inputUserName =
+                    webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+            inputUserName.clear();
+            inputUserName.sendKeys(userName);
+            logger.info("Login was inputted");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+
+    }
+
+    public void enterPasswordIntoInputPassword(String password) {
+        try {
+            WebElement inputPassword =
+                    webDriver.findElement(By.xpath(".//input[@name='password' and @placeholder='Password']"));
+            inputPassword.clear();
+            inputPassword.sendKeys(password);
+            logger.info("Password was inputted");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void clickButtonLogin() {
+        try{
+            WebElement buttonLogin = webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
+            buttonLogin.click();
+            logger.info("Button was clicked");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
     }
 }
