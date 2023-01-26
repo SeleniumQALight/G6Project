d4c26cccf791c78ae44db5565f89dbc8d9db91f8
smiian <smiyana.1989@gmail.com>
2023-01-26 21:39:17 +0200

    CommonActionWithElements

---
+++ smiianProj/src/main/java/pages/CommonActionWithElements.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class CommonActionWithElements {
+    WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+
+
+
+    public CommonActionWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+    }
+
+    protected void enterTextIntoElement(WebElement webElement, String text) {
+        try {
+            webElement.clear();
+            webElement.sendKeys(text);
+            logger.info(text + " was inputted into element");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected void clickOnElement(WebElement webElement){
+        try {
+            webElement.click();
+            logger.info("Element was clicked");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected  void printErrorAndStopTest(Exception e) {
+        logger.error("Can not work with element " + e);
+        Assert.fail("Can not work with element " + e);
+    }
+}
--- smiianProj/src/main/java/pages/ParentPage.java
+++ smiianProj/src/main/java/pages/ParentPage.java
@@ -1,20 +1,21 @@
 package pages;
 
-import org.apache.log4j.Logger;
-import org.junit.Assert;
+
 import org.openqa.selenium.WebDriver;
 
-public class ParentPage {
-    protected WebDriver webDriver;
-    protected Logger logger = Logger.getLogger(getClass());
-    public ParentPage(WebDriver webDriver) {
-        this.webDriver = webDriver;
-    }
 
-    protected  void printErrorAndStopTest(Exception e) {
-        logger.error("Can not work with element " + e);
-        Assert.fail("Can not work with element " + e);
+public class ParentPage extends CommonActionWithElements{
+    public ParentPage(WebDriver webDriver) {
+        super(webDriver);
     }
+//    protected WebDriver webDriver;
+//    protected Logger logger = Logger.getLogger(getClass());
+//    public ParentPage(WebDriver webDriver) {
+//        this.webDriver = webDriver;
+//        PageFactory.initElements(webDriver, this);  // initiate elements with xPathes
+//    }
+
+
 
 
 }
