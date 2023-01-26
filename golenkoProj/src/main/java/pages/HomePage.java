c78ad0f5f7e9c651bf03103bfd695894f95cd762
Nataliia Golenko <nataliepinchuk@gmail.com>
2023-01-26 20:13:25 +0200

    testcase logic

---
+++ golenkoProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends ParentPage{
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+    public boolean isButtonSignOutDisplayed(){
+        try {
+            return webDriver.findElement(By.xpath(".//button[text()='Sign Out']")).isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+    }
+}
