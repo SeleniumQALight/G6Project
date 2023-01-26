a621ca19a1a611e0d033b4074bc6e496716110ca
MarinaShtanko <astarohsi14@gmail.com>
2023-01-26 20:13:17 +0200

    taste case logic

--- shtankoProj/src/main/java/pages/LoginPage.java
+++ shtankoProj/src/main/java/pages/LoginPage.java
@@ -1,19 +1,56 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class LoginPage extends ParentPage{
+public class LoginPage extends ParentPage {
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
-    public void openLoginPage(){
+
+    public void openLoginPage() {
         try {
             webDriver.get("https://qa-complexapp.onrender.com/");
             logger.info("LoginPage was opened");
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Can not open Login Page" + e);
             Assert.fail("Can not open Login Page" + e);
+        }
+    }
+
+    public void enterUserNameIntoInputLogin(String userName) {
+        try {
+            WebElement inputUserName = webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+            inputUserName.clear();
+            inputUserName.sendKeys(userName);
+            logger.info("login was inputted");
+        } catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void enterPasswordIntoInputPassword(String password) {
+        try {
+            WebElement inputPassword =
+                    webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+            inputPassword.clear();
+            inputPassword.sendKeys(password);
+            logger.info("Password was entered");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void clickButtonLogin() {
+        try {
+            WebElement buttonLogin =
+                    webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
+            buttonLogin.click();
+            logger.info("Button was clicked");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
         }
     }
 }
