618ae3a3f7a314c2cb4b60587acffbd7250b61dd
komenda <wkomenda@gmail.com>
2023-01-27 17:23:34 +0200

    testCase logic

--- komendaProj/src/main/java/pages/LoginPage.java
+++ komendaProj/src/main/java/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends ParentPage {
     public LoginPage(WebDriver webDriver) {
@@ -18,5 +20,36 @@ public class LoginPage extends ParentPage {
         }
     }
 
+    public void enterUserNameIntoInputLogin(String username) {
+        try {
+            WebElement inputUserName = webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+            inputUserName.clear();
+            inputUserName.sendKeys(username);
+            logger.info("login was inputted");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void enterPasswordIntoPassword(String password) {
+        try {
+            WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+            inputPassword.click();
+            inputPassword.sendKeys(password);
+            logger.info("Password was entered");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void clickOnButtonLogin() {
+        try {
+            WebElement buttonLogin = webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
+            buttonLogin.click();
+            logger.info("Button was clicked");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
 }
 
