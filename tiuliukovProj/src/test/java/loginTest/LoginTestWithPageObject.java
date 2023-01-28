a86d1f0e3cbf7d36c3a1123fd422f8063010f356
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2023-01-28 14:00:45 +0200

    'Invalid login' test was added. 'Is button displayed' method was refactored.

--- tiuliukovProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ tiuliukovProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -10,8 +10,19 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.openLoginPage();
         loginPage.enterUserNameIntoInputLogin("qaauto");
         loginPage.enterLoginNameIntoInputPassword("123456qwerty");
-        loginPage.clickOnButtonLogin();
+        loginPage.clickOnButtonSignIn();
 
         Assert.assertTrue("Button is not displayed", homePage.isButtonSignOutDisplayed());
+    }
+
+    @Test
+    public void invalidLogin(){
+        loginPage.openLoginPage();
+        loginPage.enterUserNameIntoInputLogin("qaauto");
+        loginPage.enterLoginNameIntoInputPassword("Failed87654321");
+        loginPage.clickOnButtonSignIn();
+
+        Assert.assertFalse("Button is displayed", homePage.isButtonSignOutDisplayed());
+        Assert.assertTrue("Button is not displayed", loginPage.isButtonSignInDisplayed());
     }
 }
