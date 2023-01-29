4dbe01c715a73facd501572502c255e06c981c13
IrinaSivova <jarinka.sha@gmail.com>
2023-01-29 14:00:33 +0100

    sivova - homework 3

---
+++ sivovaProj/src/test/java/loginTest/InvalidLoginWithPageObject.java
@@ -0,0 +1,21 @@
+package loginTest;
+
+import baseTest.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvalidLoginWithPageObject extends BaseTest {
+
+    @Test
+
+    public void invalidLogin(){
+
+        loginPage.openLoginPage();
+        loginPage.enterUserNameIntoInputLogin("qaaut");
+        loginPage.enterPasswordIntoInputPassword("123456qwerty");
+        loginPage.clickButtonLogin();
+        loginPage.isErrorMessageDisplayed();
+
+        Assert.assertTrue("User is logged in with incorrect credentials", loginPage.isErrorMessageDisplayed());
+    }
+}
