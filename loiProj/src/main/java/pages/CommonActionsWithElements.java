eb3a2bb6ec4e225754a902993f797256af109928
Loi_Emmanuel <emlzoloto@gmail.com>
2023-01-29 22:19:24 +0200

    HW_2_1 Negative login test with page object was added

--- loiProj/src/main/java/pages/CommonActionsWithElements.java
+++ loiProj/src/main/java/pages/CommonActionsWithElements.java
@@ -15,28 +15,37 @@ public class CommonActionsWithElements {
         PageFactory.initElements(webDriver, this);
     }
 
-    protected void enterTextIntoElement(WebElement webElement, String text){
-        try{
+    protected void enterTextIntoElement(WebElement webElement, String text) {
+        try {
             webElement.clear();
             webElement.sendKeys(text);
             logger.info(text + " was inputted into element");
-        } catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
 
-    protected void printErrorAndStopTest(Exception e){
+    protected void printErrorAndStopTest(Exception e) {
         logger.error("Can't work with element " + e);
         Assert.fail("Can't work with element " + e);
     }
 
-    protected void clickOnElement(WebElement webElement){
+    protected void clickOnElement(WebElement webElement) {
         try {
             webElement.click();
             logger.info("element was clicked");
-        } catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
+        }
+    }
 
+    protected boolean isElementDisplayed(WebElement webElement) {
+        try {
+            logger.info("element was displayed");
+            return webElement.isDisplayed();
+        } catch (Exception e) {
+            logger.error("Can't work with element " + e);
+            return false;
         }
     }
 }
--- loiProj/src/main/java/pages/LoginPage.java
+++ loiProj/src/main/java/pages/LoginPage.java
@@ -40,4 +40,8 @@ public class LoginPage extends ParentPage {
     public void clickOnButtonLogin() {
         clickOnElement(buttonLogin);
     }
+
+    public boolean isButtonLoginDisplayed() {
+        return isElementDisplayed(buttonLogin);
+    }
 }
