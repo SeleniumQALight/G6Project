9340819b261af86a0db5c69aed1c937e9dd06588
Luydochka <luydochka08@gmail.com>
2023-01-29 23:13:11 -0300

    testCaseLogic

---
+++ muntyanProj/src/main/java/pages/ParentPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class ParentPage {
+    protected WebDriver webDriver;
+    protected Logger logger = Logger.getLogger(getClass());
+
+    public ParentPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    protected void printErrorAndStopTest(Exception e) {
+        logger.error("Can not work with element " + e);
+        Assert.fail("Can not work with element " + e);
+    }
+
+}
