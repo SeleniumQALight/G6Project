8b5bb35aa268d6a85907322509f05b7d4df81773
Luydochka <luydochka08@gmail.com>
2023-01-29 23:56:12 -0300

    Common Actions with Elements

--- muntyanProj/src/main/java/pages/ParentPage.java
+++ muntyanProj/src/main/java/pages/ParentPage.java
@@ -1,20 +1,13 @@
 package pages;
 
-import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
-public class ParentPage {
-    protected WebDriver webDriver;
-    protected Logger logger = Logger.getLogger(getClass());
+public class ParentPage extends CommonActionsWithElements {
+
 
     public ParentPage(WebDriver webDriver) {
-        this.webDriver = webDriver;
+        super(webDriver);
     }
 
-    protected void printErrorAndStopTest(Exception e) {
-        logger.error("Can not work with element " + e);
-        Assert.fail("Can not work with element " + e);
-    }
 
 }
