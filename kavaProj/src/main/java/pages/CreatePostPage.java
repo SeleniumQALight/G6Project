2b6e290a6da02ef6f5159ad325a089a041410669
Kostantin <kostiakava2@gmail.com>
2023-02-02 21:47:59 +0200

    CreatePostTest part#2

--- kavaProj/src/main/java/pages/CreatePostPage.java
+++ kavaProj/src/main/java/pages/CreatePostPage.java
@@ -8,16 +8,49 @@ import org.openqa.selenium.support.FindBy;
 public class CreatePostPage extends ParentPage {
     @FindBy(name = "title")
     private WebElement inputTitle;
+
+    @FindBy(id = "post-body")
+    private WebElement inputBody;
+
+    @FindBy(xpath = ".//button[@class='btn btn-primary']")
+    private WebElement saveButton;
+
+    @FindBy(tagName = "select")
+    private WebElement dropDownOptions;
+
     public CreatePostPage(WebDriver webDriver) {
         super(webDriver);
     }
-    public CreatePostPage checkIsRedirectToCreatePostPage(){
+
+    public CreatePostPage checkIsRedirectToCreatePostPage() {
         Assert.assertTrue("CreatePostPage is not loaded", isElementDisplayed(inputTitle));
         return this;
 
     }
-    public CreatePostPage enterTextInInputTitle(String postTitle){
+
+    public CreatePostPage enterTextInInputTitle(String postTitle) {
         enterTextInToElement(inputTitle, postTitle);
+        return this;
+    }
+
+    public CreatePostPage enterTextIntoPostBody(String bodyText) {
+        enterTextInToElement(inputBody, bodyText);
+        return this;
+    }
+
+
+    public PostPage clickOnSavePostButton() {
+        clickOnElement(saveButton);
+        return new PostPage(webDriver);
+    }
+
+    public CreatePostPage selectTextInDropDownOptions(String textInDropDown) {
+        selectTextInDropDown(dropDownOptions, textInDropDown);
+        return this;
+    }
+
+    public CreatePostPage selectValueInDropDownOptions(String valueInDropDown) {
+        selectValueInDropDown(dropDownOptions, valueInDropDown);
         return this;
     }
 }
