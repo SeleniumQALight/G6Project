ef817d782a457afddeb54d44c5c7ed545348c3de
Margo Milevska <marharitamilevska@gmail.com>
2023-02-02 22:46:14 +0200

    Hw2 - invalid login(with page object)

--- milevskaProj/src/main/java/pages/HomePage.java
+++ milevskaProj/src/main/java/pages/HomePage.java
@@ -3,7 +3,9 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+
 public class HomePage extends ParentPage{
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -14,6 +16,13 @@ public class HomePage extends ParentPage{
             return false;
         }
     }
+     public boolean isButtonSignInDisplayed(){
+        try{
+            return webDriver.findElement(By.xpath(".//button[not(@type='submit')]")).isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+     }
 }
 
 
--- milevskaProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ milevskaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -3,16 +3,27 @@ package loginTest;
 import baseTest.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginTestWithPageObject extends BaseTest {
+
     @Test
     public void validLogin(){
         loginPage.openLoginPage();
         loginPage.enterUserNameIntoInputLogin("qaauto");
         loginPage.enterPasswordIntoInputPassword("123456qwerty");
         loginPage.clickOnButtonLogin();
-
         Assert.assertTrue("Button is not displayed", homePage.isButtonSignOutDisplayed());
+    }
 
+
+    public void invalidLogin(){
+        loginPage.openLoginPage();
+        loginPage.enterUserNameIntoInputLogin("qaauto");
+        loginPage.enterPasswordIntoInputPassword("123456qwerty!");
+        loginPage.clickOnButtonLogin();
+        Assert.assertFalse("Button Sign Out is displayed", homePage.isButtonSignOutDisplayed());
+        Assert.assertTrue("Button is not displayed", homePage.isButtonSignInDisplayed());
     }
 }
