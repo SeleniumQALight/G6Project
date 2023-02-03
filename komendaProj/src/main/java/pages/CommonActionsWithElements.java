2a2ed42ebc48e8e24dee89580a418b03ad2fb8e9
komenda <wkomenda@gmail.com>
2023-02-03 16:54:39 +0200

    CreatePostTest part2

--- komendaProj/src/main/java/pages/CommonActionsWithElements.java
+++ komendaProj/src/main/java/pages/CommonActionsWithElements.java
@@ -5,9 +5,10 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class CommonActionsWithElements {
-    WebDriver webDriver;
+    protected WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
 
     public CommonActionsWithElements(WebDriver webDriver) {
@@ -49,6 +50,27 @@ public class CommonActionsWithElements {
             return false;
         }
     }
+
+    protected void selectTextInDropDown(WebElement dropDown, String visibleText){
+        try{
+            Select select = new Select(dropDown);
+            select.selectByVisibleText(visibleText);
+            logger.info(visibleText + " was select in DropDown");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected void selectValueInDropDown(WebElement dropDown, String value){
+        try {
+            Select select = new Select(dropDown);
+            select.selectByValue(value);
+            logger.info(value + " was selected in DropDown");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
 
     protected void printErrorAndStopTest(Exception e){
         logger.error("Can not work with element " + e);
--- komendaProj/src/main/java/pages/CreatePostPage.java
+++ komendaProj/src/main/java/pages/CreatePostPage.java
@@ -10,6 +10,17 @@ public class CreatePostPage extends ParentPage{
     @FindBy(name = "title")
     private WebElement inputTitle;
 
+    @FindBy(id = "post-body")
+    private WebElement inputBody;
+
+    @FindBy (xpath =".//button[@class='btn btn-primary']")
+    private WebElement buttonSavePost;
+
+//   './/select'
+    @FindBy (tagName = "select")
+    private WebElement dropDownOptions;
+
+
     public CreatePostPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -22,6 +33,26 @@ public class CreatePostPage extends ParentPage{
 
     public CreatePostPage enterTextInInputTitle(String postTitle) {
         enterTextInToElement(inputTitle, postTitle);
+        return this;
+    }
+
+    public CreatePostPage enterTextInInputBody(String bodyText) {
+        enterTextInToElement(inputBody, bodyText);
+        return this;
+    }
+
+    public PostPage clickOnSavePostButton() {
+        clickOnElement(buttonSavePost);
+        return new PostPage(webDriver);
+    }
+
+    public CreatePostPage selectTextInDropDownOptions(String textInDD) {
+        selectTextInDropDown(dropDownOptions, textInDD);
+        return this;
+    }
+
+    public CreatePostPage selectValueInDropDownOptions(String valueInDD){
+        selectValueInDropDown(dropDownOptions,valueInDD);
         return this;
     }
 }
