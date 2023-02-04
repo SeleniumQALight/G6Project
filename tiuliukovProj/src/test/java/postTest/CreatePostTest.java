788d48aa2e86c83c8ce0410a6ccc1ff21f489769
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2023-02-04 17:15:32 +0200

    HW_3 selectTextInDropDownOptionByUI method was added

--- tiuliukovProj/src/test/java/postTest/CreatePostTest.java
+++ tiuliukovProj/src/test/java/postTest/CreatePostTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 
 public class CreatePostTest extends BaseTest {
     final String POST_TITLE = "TC1_Tiuliukov";
-    final String POST_BODY = "New post successfully created.";
+    final String POST_BODY = "New post successfully created.OP";
+    final String OPTION_ALL_USERS = "Загальнодоступне";
+    final String OPTION_ONE_PERSON = "Приватне повідомлення";
+    final String OPTION_GROUP_MESSAGE = "Групове повідомлення";
 
     @Test
     public void TC1_createNewPost(){
@@ -16,7 +19,8 @@ public class CreatePostTest extends BaseTest {
                 .enterTextInInputTitle(POST_TITLE)
                 .enterTextInInputBody(POST_BODY)
                 //.selectTextInDropDownOption("Приватне повідомлення")
-                .selectValueInDropDownOptions("One Person")
+                .selectTextInDropDownOptionByUI(OPTION_ONE_PERSON)
+                //.selectValueInDropDownOptions("One Person")
                 .clickOnSaveNewPostButton()
                 .checkIsRedirectToPostPage()
                 .checkTextInSuccessMessage("New post successfully created.")
