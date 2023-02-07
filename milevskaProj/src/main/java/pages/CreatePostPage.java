4b517f58c5b2de54351c861809749dce67d5f3a4
Margo Milevska <marharitamilevska@gmail.com>
2023-02-07 13:45:30 +0200

    CreatePostPage part2

--- milevskaProj/src/main/java/pages/CreatePostPage.java
+++ milevskaProj/src/main/java/pages/CreatePostPage.java
@@ -8,6 +8,16 @@ import org.openqa.selenium.support.FindBy;
 public class CreatePostPage extends ParentPage{
     @FindBy(name = "title")
     private WebElement inputTitle;
+
+    @FindBy(id = "post-body")
+    private WebElement inputBody;
+
+    @FindBy(xpath = ".//form[@action='/create-post']//button")
+    private WebElement buttonSavePost;
+
+    @FindBy(tagName = "select")
+    private WebElement dropDownOptions;
+
     public CreatePostPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -18,6 +28,26 @@ public class CreatePostPage extends ParentPage{
     }
     public CreatePostPage enterTextInInputTitle(String postTitle){
         enterTextInToElement(inputTitle, postTitle);
+        return this;
+    }
+
+    public CreatePostPage enterTextInInputBody(String postTitle){
+        enterTextInToElement(inputBody, postTitle);
+        return this;
+    }
+
+    public PostPage clickOnSavePostButton(){
+        clickOnElement(buttonSavePost);
+        return new PostPage(webDriver);
+    }
+
+    public CreatePostPage selectTextInDropDownOptions(String textInDD) {
+        selectTextInDropDown(dropDownOptions, textInDD);
+        return this;
+    }
+
+    public CreatePostPage selectValueInDropDownOptions(String valueInDD){
+        selectValueInDropDown(dropDownOptions, valueInDD);
         return this;
     }
 }
