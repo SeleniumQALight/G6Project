dfd5bae49a832d33a4467696f9bcf827078a4eec
Nataliia Golenko <nataliepinchuk@gmail.com>
2023-02-07 21:43:27 +0200

    CreatePostTest Part3

--- golenkoProj/src/main/java/pages/MyProfilePage.java
+++ golenkoProj/src/main/java/pages/MyProfilePage.java
@@ -1,13 +1,18 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class MyProfilePage extends ParentPage{
     @FindBy (xpath = ".//img[@class='avatar-small']")
     private WebElement avatar;
+
+    private String titlePost = ".//*[text()='%s']";
 
     public MyProfilePage(WebDriver webDriver) {
         super(webDriver);
@@ -17,6 +22,15 @@ public class MyProfilePage extends ParentPage{
         //TODO check URL
         Assert.assertTrue("MyProfilePage is not loaded",
                 isElementDisplayed(avatar));
+        return this;
+    }
+
+    public List<WebElement> getPostsListWithTitle (String title){
+        return webDriver.findElements(By.xpath(String.format(titlePost, title)));
+    }
+
+    public MyProfilePage checkPostWasCreated(String postTitle) {
+        Assert.assertEquals("Number of posts with title", 1, getPostsListWithTitle(postTitle).size());
         return this;
     }
 }
