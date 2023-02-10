eb8cb1e68080d8c38aaa7829e4645c4bc5fa6825
Kostantin Kava <kostiakava2@gmail.com>
2023-02-10 16:50:26 +0200

    Methods

--- kavaProj/src/main/java/pages/CommonActionsWithElements.java
+++ kavaProj/src/main/java/pages/CommonActionsWithElements.java
@@ -2,10 +2,8 @@ package pages;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,6 +11,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
@@ -104,6 +103,59 @@ public class CommonActionsWithElements {
         Assert.fail("Can not work with element " + e);
 
     }
+
+    public void usersPressesKeyEnterTime(int numberOfTimes) {
+        Actions actions = new Actions(webDriver);
+        for (int i = 0; i < numberOfTimes; i++) {
+            actions.sendKeys(Keys.ENTER).build().perform();
+        }
+    }
+
+    public void usersPressesKeyTabTime(int numberOfTimes) {
+        Actions actions = new Actions(webDriver);
+        for (int i = 0; i < numberOfTimes; i++) {
+            actions.sendKeys(Keys.TAB).build().perform();
+        }
+
+    }
+
+    public void usersPressesKeyTime(Keys keys, int numberOfTimes) {
+        Actions actions = new Actions(webDriver);
+        for (int i = 0; i < numberOfTimes; i++) {
+            actions.sendKeys(keys).build().perform();
+        }
+
+    }
+
+    public void userOpensNewTab() {
+        ((JavascriptExecutor)webDriver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<> (webDriver.getWindowHandles());
+        webDriver.switchTo().window(tabs.get(1));
+    }
+//
+//    метод moveToElement (аналог скрола )
+//
+//    WebElement element = driver.findElement(By.id("my-id"));
+//    Actions actions = new Actions(driver);
+//actions.moveToElement(element);
+//actions.perform();
+//
+//—————————-
+//    метод скрола з використанням javaScript
+//
+//    JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("javascript:window.scrollBy(250,350)");
+//
+//—————————-
+//    Емуляція натискання PageDown
+//
+//WebElement.sendKeys(Keys.DOWN);
+//
+//—————————-
+//    скрол до елемента з javaScript
+//
+//            webElement = driver.findElement(By.xpath("bla-bla-bla"));
+//((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", webElement);
 
 
 }
