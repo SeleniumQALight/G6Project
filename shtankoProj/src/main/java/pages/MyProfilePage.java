b0964fa55b87b84fe1c2d7bac39e3e854a61e8ce
MarinaShtanko <astarohsi14@gmail.com>
2023-02-15 19:22:08 +0200

    homework part2

--- shtankoProj/src/main/java/pages/MyProfilePage.java
+++ shtankoProj/src/main/java/pages/MyProfilePage.java
@@ -37,6 +37,10 @@ public class MyProfilePage extends ParentPage{
         Assert.assertEquals("Number of posts with title", 1,getPostListWithTitle(postTitle).size());
         return this;
     }
+    public PostPage clickOnPostPage (String postTitle){
+        clickOnElement(String.format(titlePost, postTitle));
+        return new PostPage(webDriver);
+    }
 
     public MyProfilePage deletePostWithTitleTillPresent(String postTitle) {
         List<WebElement>listOfPosts = getPostListWithTitle(postTitle); //отримуємо список всіх постів з тайтлом який ми створити при тестуванні
--- shtankoProj/src/test/java/postTest/EditPostTest.java
+++ shtankoProj/src/test/java/postTest/EditPostTest.java
@@ -1,7 +1,62 @@
 package postTest;
 
 import baseTest.BaseTest;
+import libs.Util;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class EditPostTest extends BaseTest {
+    final String POST_TITLE = "tc2_shtanko_" + Util.getDateAndTimeFormatted();
+    final String POST_TITLE_EDIT = "TC2_SHTANKO_" + Util.getDateAndTimeFormatted();
+    final String POST_BODY = "TC2_Shtanko post body";
+    @Before
+    public void tc2_BeforeCreatePost(){
+        homePage
+                .openHomePage()
+                .getHeaderElement().clickOnCreatePostButton()
+                .checkIsRedirectToCreatePostPage()
+                .enterTextInInputTitle(POST_TITLE)
+                .enterTextInInputBody(POST_BODY)
+                .selectTextInDropDownOptions("Приватне повідомлення")
+                .clickOnSavePostButton()
+                .checkIsRedirectToPostPage()
+                .checkTextInSuccessMessage("New post successfully created.")
+                .checkTextInNewTitle(POST_TITLE)
+                .checkNotePost()
+                .checkStatusPost("One Person")
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkUserName("qaauto")
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWasCreated(POST_TITLE)
+        ;
+    }
+    @Test
+    public void tc2_TestEditPost(){
+        homePage
+                .openHomePage()
+                .getHeaderElement().clickOnMyProfileButton()
+                .clickOnPostPage(POST_TITLE)
+                .checkTextInNewTitle(POST_TITLE)
+                .clickOnEditButton()
+                .enterEditTextInInputTitle(POST_TITLE_EDIT)
+                .clickOnSaveUpdateButton()
+                .checkTextInSuccessUpdatedMessage("Post successfully updated.")
+                .clickOnBackPostButton()
+                .checkTextInNewTitle(POST_TITLE_EDIT)
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWasCreated(POST_TITLE_EDIT)
+        ;
+    }
+    @After
+    public void tc2_DeletePost(){
+        homePage
+                .openHomePage()
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostWithTitleTillPresent(POST_TITLE_EDIT);
+
+    }
 
 }
