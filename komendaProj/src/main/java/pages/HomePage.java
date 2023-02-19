8a77a868680f9033cd34e2c59c27d8df738b8a43
komenda <wkomenda@gmail.com>
2023-02-19 16:49:30 +0200

    HW#3

--- komendaProj/src/main/java/pages/HomePage.java
+++ komendaProj/src/main/java/pages/HomePage.java
@@ -3,8 +3,6 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import pages.elements.HeaderElement;
 
 public class HomePage extends ParentPage {
@@ -58,10 +56,6 @@ public class HomePage extends ParentPage {
     public CreatePostPage clickOnCreatePostButton() {
         clickOnElement(headerElement.getButtonCreatePost());
         return new CreatePostPage(webDriver);
-    }
-
-    public HeaderElement getHeaderElement() {
-          return headerElement;
     }
 
 }
--- komendaProj/src/test/java/postTest/CreatePostTest.java
+++ komendaProj/src/test/java/postTest/CreatePostTest.java
@@ -1,6 +1,7 @@
 package postTest;
 
 import baseTest.BaseTest;
+import libs.TestData;
 import libs.Util;
 import org.junit.After;
 import org.junit.Test;
@@ -21,8 +22,12 @@ public class CreatePostTest  extends BaseTest {
                 .clickOnSavePostButton()
             .checkIsRedirectToPostPage()
                 .checkTextInSuccessMessage("New post successfully created.")
+                .checkIsTitleDisplayed(POST_TITLE) //HW#3 - Перевірити що ви бачите створений Title на сторінці PostPage
+                .checkIsNoteDisplayed("Note: This post was written for") //HW#3 - Перевірити що присутня лейба Note: This post was written for
+                .checkIsUnderLineText("One Person") //HW#3 - Перевірити що присутня що в body присутній текст з "підкресленням" і той текст "One Person"
                 .getHeaderElement().clickOnMyProfileButton()
             .checkIsRedirectToMyProfilePage()
+                .checkIsCorrectLoginDisplayed(TestData.VALID_LOGIN) //HW#3 - Перевірити що присутня на сторінці MyProfile, перевірити що бачимо Юзера, яким залогінитися.
                 .checkPostWasCreated(POST_TITLE)
         ;
     }
