32bf93538f65f423569146a609531aba7ab2d822
AntonBerezhnyi <xaosdhx@gmail.com>
2023-02-21 21:36:20 +0200

    properties

--- berezhnyiProj/src/main/java/pages/ParentPage.java
+++ berezhnyiProj/src/main/java/pages/ParentPage.java
@@ -11,7 +11,7 @@ abstract public class ParentPage extends CommonActionsWithElements {
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
-        baseURL = "https://[env]-complexapp.onrender.com".replace("[env]", System.getProperty("env", "qa"));
+        baseURL = configProperties.base_url().replace("[env]", System.getProperty("env", "qa"));
     }
 
     protected void waitChatToHide(){
