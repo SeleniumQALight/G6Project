d45d18b1ff404d9a3dad512a8d79615568c69062
smiian <smiyana.1989@gmail.com>
2023-02-21 20:33:01 +0200

    smiian-HW-02-21-browsers

--- smiianProj/src/test/java/BaseTest/BaseTest.java
+++ smiianProj/src/test/java/BaseTest/BaseTest.java
@@ -9,6 +9,10 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.PostEditPage;
@@ -31,9 +35,10 @@ public class BaseTest {
     @Before
     public void setUp(){
         logger.info("------ " + testName.getMethodName()+ " was started------");    // для гарного логування даних по кейсу на початку логу
+        webDriver = initDriver();
 
-        WebDriverManager.chromedriver().setup();   // запуск браузера Хром
-        webDriver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();   // запуск браузера Хром
+//        webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();    // відкриваємо вікно на весь екран
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
@@ -54,6 +59,30 @@ public class BaseTest {
     }
 
     @Rule
-    public TestName testName = new TestName();
+    public TestName testName = new TestName();  // працює з 33 та 54
+
+    private WebDriver initDriver() {                       //
+        String browser = System.getProperty("browser");
+        if ((browser==null) || "chrome".equalsIgnoreCase(browser)){
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else if ("firefox".equalsIgnoreCase(browser)) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else if ("safari".equalsIgnoreCase(browser)) {
+            WebDriverManager.safaridriver().setup();
+            webDriver = new SafariDriver();
+        } else if ("edge".equalsIgnoreCase(browser)){
+            WebDriverManager.edgedriver().setup();
+            webDriver=new EdgeDriver();
+        } else if ("ie".equalsIgnoreCase(browser)) {
+            //WebDriverManager.iedriver().setup();
+            // in most cases 32bit version is needed
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        }
+
+        return webDriver;
+    }
 
 }
