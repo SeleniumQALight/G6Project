2b27c910c0b2090d2a59ba0b5595fad20621df98
IrinaSivova <jarinka.sha@gmail.com>
2023-02-21 20:32:39 +0200

    sivova - classwork 21-02 part 2

--- sivovaProj/src/test/java/baseTest/BaseTest.java
+++ sivovaProj/src/test/java/baseTest/BaseTest.java
@@ -8,6 +8,10 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -25,8 +29,7 @@ public class BaseTest {
     public void setUp(){
         logger.info("------ " + testName.getMethodName()+ " was started -------");
         WebDriverManager.chromedriver().setup();
-        webdriver = new ChromeDriver();
-        webdriver.manage().window().maximize();
+        webdriver = initDriver();
         webdriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         loginPage = new LoginPage(webdriver);
         homePage = new HomePage(webdriver);
@@ -43,5 +46,27 @@ public class BaseTest {
 
     @Rule
     public TestName testName = new TestName();
+
+    private WebDriver initDriver(){
+        String browser = System.getProperty("browser");
+        if ((browser == null) || "chrome".equalsIgnoreCase(browser)){
+            WebDriverManager.chromedriver().setup();
+            webdriver = new ChromeDriver();
+        } else if ("firefox".equalsIgnoreCase(browser)) {
+            WebDriverManager.firefoxdriver().setup();
+            webdriver = new FirefoxDriver();
+        } else if ("safari".equalsIgnoreCase(browser)) {
+            WebDriverManager.safaridriver().setup();
+            webdriver = new SafariDriver();
+        } else if ("ie".equalsIgnoreCase(browser)) {
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        } else if ("edge".equalsIgnoreCase(browser)) {
+            WebDriverManager.edgedriver().setup();
+            webdriver = new EdgeDriver();
+        }
+
+        return webdriver;
+    }
 
 }
