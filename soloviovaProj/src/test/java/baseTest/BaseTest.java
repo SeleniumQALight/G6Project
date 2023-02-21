0d321506f713ecb7a5a9543a715e502808e51d00
yuliia soloviova <julysoloviova@gmail.com>
2023-02-21 13:22:03 -0400

    Junit rule.

--- soloviovaProj/src/test/java/baseTest/BaseTest.java
+++ soloviovaProj/src/test/java/baseTest/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
@@ -21,6 +23,7 @@ public class BaseTest {
 
     @Before
     public void setup() {
+        logger.info("--------- " + testName.getMethodName()+" is started-----");
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
@@ -34,6 +37,11 @@ public class BaseTest {
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser was closed.");
+        logger.info("--------- " + testName.getMethodName()+" is finished-----");
+
     }
+
+    @Rule
+    public TestName testName = new TestName();
 
 }
