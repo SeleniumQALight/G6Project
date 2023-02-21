8a7929019b2c077e1106e7fe7a3499193928b3be
Nataliia Golenko <nataliepinchuk@gmail.com>
2023-02-21 21:36:06 +0200

    properties

--- golenkoProj/src/main/java/pages/ParentPage.java
+++ golenkoProj/src/main/java/pages/ParentPage.java
@@ -13,7 +13,7 @@ abstract public class ParentPage extends CommonActionsWithElements {
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
-        base_url = "https://[env]-complexapp.onrender.com"
+        base_url = configProperties.base_url()
                 .replace("[env]", System.getProperty("env", "qa"));
     }
 
