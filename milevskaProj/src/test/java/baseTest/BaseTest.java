d441e326fa39b1ac6b99abcf74c3f25b4effd0db
Margo Milevska <marharitamilevska@gmail.com>
2023-02-23 09:58:46 +0200

    jUnit rule

--- milevskaProj/src/test/java/baseTest/BaseTest.java
+++ milevskaProj/src/test/java/baseTest/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
@@ -19,6 +21,7 @@ public class BaseTest {
 
     @Before
     public void setUp(){
+        logger.info("------" + testName.getMethodName() + " was started----");
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
@@ -31,5 +34,9 @@ public class BaseTest {
     public void tearDown(){
         webDriver.quit();
         logger.info("Browser was closed");
+        logger.info("---" + testName.getMethodName() + " was ended---");
     }
+
+    @Rule
+    public TestName testName = new TestName();
 }
