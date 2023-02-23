b6eabb49fcb06b153d58aa3d68a213608b6a30cd
Loi_Emmanuel <emlzoloto@gmail.com>
2023-02-23 19:49:11 +0200

    hiddenConfig

--- loiProj/src/main/java/libs/TestData.java
+++ loiProj/src/main/java/libs/TestData.java
@@ -1,6 +1,9 @@
 package libs;
 
+import org.aeonbits.owner.ConfigFactory;
+
 public class TestData {
-    public final static String VALID_LOGIN = "qaauto";
-    public final static String VALID_PASSWORD = "123456qwerty";
+    public static ConfigHiddenProperties configHiddenProperties = ConfigFactory.create(ConfigHiddenProperties.class);
+    public final static String VALID_LOGIN = System.getProperty("login", configHiddenProperties.login());
+    public final static String VALID_PASSWORD = System.getProperty("password", configHiddenProperties.password());
 }
