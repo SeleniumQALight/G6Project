cef268ebe1ac811d6992195084a837d514c38f7b
olga-pustovoit <64926503+olga-pustovoit@users.noreply.github.com>
2023-02-23 15:33:17 +0200

    properties

--- pustovoitProj/src/main/java/pages/ParentPage.java
+++ pustovoitProj/src/main/java/pages/ParentPage.java
@@ -11,7 +11,7 @@ abstract public class ParentPage extends CommonActionsWithElements{
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
-        base_url = base_url = "https://[env]-complexapp.onrender.com"
+        base_url = base_url = configProperties.base_url()
                 .replace("[env]", System.getProperty("env","qa"));
     }
 
