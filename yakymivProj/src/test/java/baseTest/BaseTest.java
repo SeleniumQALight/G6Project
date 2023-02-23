87651e38250349e7d7fae6c9bb0737ce85d49169
IvanYakymiv <yakymiv.ivan@gmail.com>
2023-02-23 15:33:33 +0100

    junit rule

--- yakymivProj/src/test/java/baseTest/BaseTest.java
+++ yakymivProj/src/test/java/baseTest/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
@@ -20,6 +22,7 @@ public class BaseTest {
 
     @Before
     public void setUp() {
+        logger.info("--------" + testName.getMethodName() + " was started--------");
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
@@ -34,5 +37,10 @@ public class BaseTest {
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser is closed");
+        logger.info("--------" + testName.getMethodName() + " was ended--------");
     }
+
+
+    @Rule
+    public TestName testName = new TestName();
 }
