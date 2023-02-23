f951578d6545ddeb4e13dd557721c91659af5cbb
IrinaSivova <jarinka.sha@gmail.com>
2023-02-23 22:17:30 +0200

    sivova - classwork 23-02 part 2

--- sivovaProj/src/main/java/libs/TestData.java
+++ sivovaProj/src/main/java/libs/TestData.java
@@ -6,7 +6,7 @@ public class TestData {
     public static ConfigHiddenProperties configHiddenProperties = ConfigFactory.create(ConfigHiddenProperties.class);
 
     public final static String VALID_LOGIN = System.getProperty("login", configHiddenProperties.login());
-    public final static String VALID_PASSWORD = System.getProperty("password", configHiddenProperties.login());
+    public final static String VALID_PASSWORD = System.getProperty("password", configHiddenProperties.password());
     public final static String INVALID_USERNAME = "tr";
     public final static String ERROR_MESSAGE_USERNAME = "Username must be at least 3 characters.";
     public final static String INVALID_PASSWORD = "123";
