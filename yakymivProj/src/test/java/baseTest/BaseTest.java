d47a71174fd7ad42ad656e084917c07301597403
IvanYakymiv <yakymiv.ivan@gmail.com>
2023-02-23 16:40:23 +0100

    browsers

--- yakymivProj/src/test/java/baseTest/BaseTest.java
+++ yakymivProj/src/test/java/baseTest/BaseTest.java
@@ -8,6 +8,10 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -23,8 +27,7 @@ public class BaseTest {
     @Before
     public void setUp() {
         logger.info("--------" + testName.getMethodName() + " was started--------");
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         loginPage = new LoginPage(webDriver);
@@ -40,7 +43,29 @@ public class BaseTest {
         logger.info("--------" + testName.getMethodName() + " was ended--------");
     }
 
-
     @Rule
     public TestName testName = new TestName();
+
+    private WebDriver initDriver(){
+        String browser = System.getProperty("browser");
+        if ((browser == null) || "chrome".equals(browser)){
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        }else if ("firefox".equalsIgnoreCase(browser)) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }else if ("safari".equalsIgnoreCase(browser)) {
+            WebDriverManager.safaridriver().setup();
+            webDriver = new SafariDriver();
+        }else if ("edge".equalsIgnoreCase(browser)){
+            WebDriverManager.edgedriver().setup();
+            webDriver=new EdgeDriver();
+        }else if ("ie".equalsIgnoreCase(browser)) {
+            //WebDriverManager.iedriver().setup();
+            // in most cases 32bit version is needed
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        }
+        return webDriver;
+    }
 }
