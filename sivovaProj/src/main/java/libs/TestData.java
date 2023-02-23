25d76b77d30ec22473988bfe3330dee0c8d68d97
IrinaSivova <jarinka.sha@gmail.com>
2023-02-23 19:49:24 +0200

    sivova - classwork 23-02

--- sivovaProj/src/main/java/libs/TestData.java
+++ sivovaProj/src/main/java/libs/TestData.java
@@ -1,12 +1,18 @@
 package libs;
 
+import org.aeonbits.owner.ConfigFactory;
+
 public class TestData {
-    public final static String VALID_LOGIN = "qaauto";
-    public final static String VALID_PASSWORD = "123456qwerty";
+    public static ConfigHiddenProperties configHiddenProperties = ConfigFactory.create(ConfigHiddenProperties.class);
+
+    public final static String VALID_LOGIN = System.getProperty("login", configHiddenProperties.login());
+    public final static String VALID_PASSWORD = System.getProperty("password", configHiddenProperties.login());
     public final static String INVALID_USERNAME = "tr";
     public final static String ERROR_MESSAGE_USERNAME = "Username must be at least 3 characters.";
     public final static String INVALID_PASSWORD = "123";
     public final static String INVALID_EMAIL = "test.com";
     public final static String ERROR_MESSAGE_EMAIL = "You must provide a valid email address.";
     public final static String ERROR_MESSAGE_PASSWORD = "Password must be at least 12 characters.";
+
+
 }
