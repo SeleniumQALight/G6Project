ee9da2c2fc657d5d0735b2e74315792ce654f7fb
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2023-02-25 13:52:42 +0200

    RegistrationErrorsTest was extended.

--- tiuliukovProj/src/test/java/registrationTest/RegistrationErrorsTest.java
+++ tiuliukovProj/src/test/java/registrationTest/RegistrationErrorsTest.java
@@ -10,15 +10,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class RegistrationErrorsTest extends BaseTest {
-    final static String ERROR_USERNAME = "Username must be at least 3 characters.";
-    final static String ERROR_EMAIL = "You must provide a valid email address.";
-    final static String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final static String ERROR_USERNAME_SHORT = "Username must be at least 3 characters.";
+    final static String ERROR_USERNAME_LONG = "Username cannot exceed 30 characters.";
+    final static String ERROR_USERNAME_INVALID_CHARACTER = "Username can only contain letters and numbers.";
+    final static String ERROR_EMAIL_INVALID = "You must provide a valid email address.";
+    final static String ERROR_PASSWORD_SHORT = "Password must be at least 12 characters.";
+    final static String ERROR_PASSWORD_LONG = "Password cannot exceed 50 characters.";
     final static String ERROR_ALREADY_EXIST = "That username is already taken.";
     final static String SEMICOLON = ";";
     final static String COMMA = ",";
     final static String SHORT_USER_NAME = "tr";
 
-    String expectedErrors = ERROR_USERNAME + COMMA + ERROR_EMAIL + COMMA + ERROR_PASSWORD;
+    String expectedErrors = ERROR_USERNAME_SHORT + COMMA + ERROR_EMAIL_INVALID + COMMA + ERROR_PASSWORD_SHORT;
 
     @Test
     @Parameters(method = "provideParameters")
@@ -36,8 +39,11 @@ public class RegistrationErrorsTest extends BaseTest {
 
     public static Object[][] provideParameters() {
         return new Object[][] {
-                new Object[] {"tr", "ttt", "ttt", ERROR_USERNAME + COMMA + ERROR_EMAIL + COMMA + ERROR_PASSWORD},
-                new Object[] {"un", "email@test.com", "ppp", ERROR_USERNAME + COMMA + ERROR_PASSWORD}
+                new Object[] {"tr", "ttt", "ttt", ERROR_USERNAME_SHORT + COMMA + ERROR_EMAIL_INVALID + COMMA + ERROR_PASSWORD_SHORT},
+                new Object[] {"un", "email@test.com", "ppp", ERROR_USERNAME_SHORT + COMMA + ERROR_PASSWORD_SHORT},
+                new Object[] {"КирилицаЛогин", "email@test.com", "Password", ERROR_USERNAME_INVALID_CHARACTER},
+                new Object[] {"UserName!@#$%^&", "email@test.com", "Password", ERROR_USERNAME_INVALID_CHARACTER},
+                new Object[] {"31CharactersUserNameQwertyuiopa", "email@test.com", "51CharactersUserPasswordQwertyuiopasdfghjklzxcvbn51", ERROR_USERNAME_LONG + COMMA + ERROR_PASSWORD_LONG}
         };
     }
 }
