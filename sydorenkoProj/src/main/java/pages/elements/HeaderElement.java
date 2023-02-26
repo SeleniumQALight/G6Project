4b1e57f0b29ebca0c1167c47326953261d6b9321
EugenSydorenko <eugensydorenko@gmail.com>
2023-02-26 14:25:49 +0200

    HW5

--- sydorenkoProj/src/main/java/pages/elements/HeaderElement.java
+++ sydorenkoProj/src/main/java/pages/elements/HeaderElement.java
@@ -3,7 +3,6 @@ package pages.elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.CommonActionsWithElements;
 import pages.CreatePostPage;
 import pages.MyProfilePage;
@@ -25,7 +24,6 @@ public class HeaderElement extends CommonActionsWithElements {
         return new MyProfilePage(webDriver);
     }
     public boolean isButtonSignOutDisplayed() {
-        webDriverWait15.until(ExpectedConditions.visibilityOf(buttonSignOut));
         return isElementDisplayed(buttonSignOut);
     }
     public CreatePostPage clickOnCreatePostButton() {
--- sydorenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ sydorenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,11 +1,15 @@
 package loginTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
+@RunWith(JUnitParamsRunner.class)
 public class LoginTestWithPageObject extends BaseTest {
 
     @Test
@@ -28,6 +32,25 @@ public class LoginTestWithPageObject extends BaseTest {
         assertFalse("Button is not displayed", homePage.getHeaderElement().isButtonSignOutDisplayed());
         assertTrue("Button is not displayed", loginPage.isButtonSignInDisplayed());
         assertTrue("Button is not displayed", (!homePage.getHeaderElement().isButtonSignOutDisplayed() & loginPage.isButtonSignInDisplayed()));
+    }
+    @Test
+    @Parameters(method = "provideParameters")
+    @TestCaseName("inValidLoginParameters: login = {0}, password = {1}")
+    public void inValidLoginWithParameters(String login, String password) {
+        loginPage.openLoginPage();
+        loginPage.enterUserNameIntoInputLogin(login);
+        loginPage.enterPasswordIntoInputPassword(password);
+        loginPage.clickOnButtonLogin();
+
+        assertFalse("Button is not displayed", homePage.getHeaderElement().isButtonSignOutDisplayed());
+        assertTrue("Button is not displayed", loginPage.isButtonSignInDisplayed());
+        assertTrue("Button is not displayed", (!homePage.getHeaderElement().isButtonSignOutDisplayed() & loginPage.isButtonSignInDisplayed()));
+    }
+    public static Object[][] provideParameters() {
+        return new Object[][]{
+                new Object[]{"qaauto", "1123456qwerty"},
+                new Object[]{"qaautoo", "1123456qwertyy"},
+        };
     }
 
 }
