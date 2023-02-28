68f7b726825994f3a0a7c525be0df09e83cbbcc5
Loi_Emmanuel <emlzoloto@gmail.com>
2023-02-28 21:22:13 +0200

    Work with Excel

--- loiProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ loiProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,8 +1,14 @@
 package loginTest;
 
 import baseTest.BaseTest;
+import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static pages.CommonActionsWithElements.configProperties;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
@@ -10,6 +16,17 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.openLoginPage();
         loginPage.enterUserNameIntoInputLogin("qaauto");
         loginPage.enterPasswordIntoInputPassword("123456qwerty");
+        loginPage.clickOnButtonLogin();
+
+        Assert.assertTrue("Button isn't displayed", homePage.getHeaderElement().isButtonSignOutDisplayed());
+    }
+
+    @Test
+    public void validLoginWithExcel() throws IOException {
+        Map<String, String> dataForValidLogin = ExcelDriver.getData(configProperties.DATA_FILE(), "validLogOn");
+        loginPage.openLoginPage();
+        loginPage.enterUserNameIntoInputLogin(dataForValidLogin.get("login"));
+        loginPage.enterPasswordIntoInputPassword(dataForValidLogin.get("pass"));
         loginPage.clickOnButtonLogin();
 
         Assert.assertTrue("Button isn't displayed", homePage.getHeaderElement().isButtonSignOutDisplayed());
