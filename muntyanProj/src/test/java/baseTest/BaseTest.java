a1e85c3c09308c55abfe7e47d8bc92ad61d86630
Luydochka <luydochka08@gmail.com>
2023-02-28 10:51:01 -0300

    junit Rule

--- muntyanProj/src/test/java/baseTest/BaseTest.java
+++ muntyanProj/src/test/java/baseTest/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
@@ -22,6 +24,7 @@ public class BaseTest {
 
     @Before
     public void setUp() {
+        logger.info("--------- " + testName.getMethodName() + " was started ----------");
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
@@ -35,7 +38,11 @@ public class BaseTest {
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser was closed");
+        logger.info("-------- " + testName.getMethodName() + " was ended -----------");
     }
+
+    @Rule
+    public TestName testName = new TestName();
 
 
 }
