f5aee70477c0f500a95e608814ae3a7a9c50f549
nataliiakirichenko <nata.solari.n@gmail.com>
2023-03-03 11:50:42 +0300

    Hidden config

--- kirichenkoProj/src/main/java/libs/TestData.java
+++ kirichenkoProj/src/main/java/libs/TestData.java
@@ -1,8 +1,11 @@
 package libs;
 
+import org.aeonbits.owner.ConfigFactory;
+
 public class TestData {
-    public final static String VALID_LOGIN = "qaauto";
-    public final static String VALID_PASSWORD = "123456qwerty";
+    public static ConfigHiddenProperties configHiddenProperties = ConfigFactory.create(ConfigHiddenProperties.class);
+    public final static String VALID_LOGIN = System.getProperty("login", configHiddenProperties.login());
+    public final static String VALID_PASSWORD = System.getProperty("password", configHiddenProperties.password());
 
     public final static String INVALID_LOGIN = "Qqqq";
 
