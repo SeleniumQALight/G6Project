4b37af1f4753e54d40c7370d3dbbff6dd1511bac
MarinaShtanko <astarohsi14@gmail.com>
2023-03-09 19:47:18 +0200

    suits

--- shtankoProj/src/test/java/loginTest/LoginTestWhitPageObject.java
+++ shtankoProj/src/test/java/loginTest/LoginTestWhitPageObject.java
@@ -1,8 +1,10 @@
 package loginTest;
 import baseTest.BaseTest;
+import categories.SmokeTestFilter;
 import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
 import java.util.Map;
@@ -12,6 +14,7 @@ import static pages.CommonActionsWithElement.configProperties;
 public class LoginTestWhitPageObject extends BaseTest {
 
     @Test
+    @Category(SmokeTestFilter.class)
     public void validLoginWhitExcel() throws IOException {
         Map<String, String> dataForValidLogin = ExcelDriver.getData(configProperties.DATA_FILE(), "validLogOn");
         loginPage.openLoginPage();
