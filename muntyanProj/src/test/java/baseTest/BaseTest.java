7ae6a02ae99b7f1957d33582cef3bdb83e8bba17
Luydochka <luydochka08@gmail.com>
2023-03-12 12:14:53 -0300

    browsers

--- muntyanProj/src/test/java/baseTest/BaseTest.java
+++ muntyanProj/src/test/java/baseTest/BaseTest.java
@@ -8,6 +8,9 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.MyProfilePage;
@@ -25,8 +28,7 @@ public class BaseTest {
     @Before
     public void setUp() {
         logger.info("--------- " + testName.getMethodName() + " was started ----------");
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         loginPage = new LoginPage(webDriver);
@@ -43,6 +45,25 @@ public class BaseTest {
 
     @Rule
     public TestName testName = new TestName();
+
+    private WebDriver initDriver() {
+        String browser = System.getProperty("browser");
+        if ((browser == null ) || "chrome".equalsIgnoreCase(browser)){
+    WebDriverManager.chromedriver().setup();
+    webDriver = new ChromeDriver();
+
+        }else if ("firefox".equalsIgnoreCase(browser)){
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }else if ("edge".equalsIgnoreCase(browser)){
+            WebDriverManager.edgedriver().setup();
+            webDriver = new EdgeDriver();
+        }else if ("ie".equalsIgnoreCase(browser)) {
+            WebDriverManager.iedriver().arch32().setup();
+            webDriver = new InternetExplorerDriver();
+        }
+    return webDriver;
+    }
 
 
 }
