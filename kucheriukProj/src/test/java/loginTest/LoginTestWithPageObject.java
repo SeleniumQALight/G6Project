2fcfde324d72560a20396762adf705d05ce04119
ludmilakucheriuk <lusiya.dep@gmail.com>
2023-03-13 15:36:42 +0200

    Allure

--- kucheriukProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ kucheriukProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -2,6 +2,7 @@ package loginTest;
 
 import baseTest.BaseTest;
 import categories.SmokeTestFilter;
+import io.qameta.allure.*;
 import libs.ExcelDriver;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
@@ -16,13 +17,22 @@ import java.util.Map;
 
 import static pages.CommonActionsWithElements.configProperties;
 
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 @RunWith(JUnitParamsRunner.class)
 @Category(SmokeTestFilter.class)
 public class LoginTestWithPageObject extends BaseTest {
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
     @Test
     public void validLogin(){
         loginPage.openLoginPage();
-        loginPage.enterUserNameIntoInputLogin("qaauto");
+        loginPage.enterUserNameIntoInputLogin("qaaauto");
         loginPage.enterPasswordIntoInputPassword("123456qwerty");
         loginPage.clickOnButtonLogin();
 
