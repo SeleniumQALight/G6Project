de5a7c22191f46565dd6be63ee8008e7ee891ffd
AntonBerezhnyi <xaosdhx@gmail.com>
2023-03-16 21:37:04 +0200

    api test added 1

---
+++ berezhnyiProj/src/test/java/apiTests/ApiTests.java
@@ -0,0 +1,25 @@
+package apiTests;
+
+import api.EndPoints;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTests {
+    final String USER_NAME = "autoapi";
+    @Test
+    public void getPostByUserTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                ;
+
+
+    }
+}
