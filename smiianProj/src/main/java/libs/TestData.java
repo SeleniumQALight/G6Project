6ebb2a67990b4690942f67b764d6a4369e88eb69
smiian <smiyana.1989@gmail.com>
2023-03-17 14:10:30 +0200

    smiian-03-16

--- smiianProj/src/main/java/libs/TestData.java
+++ smiianProj/src/main/java/libs/TestData.java
@@ -4,8 +4,13 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class TestData {     //створюємо данні, які будуть використовуватись багато разів
     public static ConfigHiddenProperties configHiddenProperties = ConfigFactory.create(ConfigHiddenProperties.class);
-    public final static String VALID_LIGIN = System.getProperty("login", configHiddenProperties.login());           // назва капсом для константи
-    public final static String VALID_PASSWORD = System.getProperty("password", configHiddenProperties.password());   //  System.getProperty вставляє пароль "password"
+
+    public final static String VALID_LIGIN =                                      // назва капсом для константи
+            System.getProperty("login", configHiddenProperties.login());      // login() - пов'язує з файлом ConfigHiddenProperties, а він
+                                                                                   // в свою чергу, вов'язує з файлом hiddenConfig.properties, де вказані дані
+
+    public final static String VALID_PASSWORD =
+            System.getProperty("password", configHiddenProperties.password());   //  System.getProperty вставляє пароль "password"
                                                                                                    // а якщо небуде що вставляти візьме configHiddenProperties.password()
 
 }
