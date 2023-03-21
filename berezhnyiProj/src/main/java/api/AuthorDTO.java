2c60331884d1c623273ec7ed1ffcded9a76982a9
AntonBerezhnyi <xaosdhx@gmail.com>
2023-03-21 21:49:50 +0200

    DTO

---
+++ berezhnyiProj/src/main/java/api/AuthorDTO.java
@@ -0,0 +1,38 @@
+package api;
+
+public class AuthorDTO {
+    String username;
+    String avatar;
+
+    public AuthorDTO(){
+
+    }
+
+    public AuthorDTO(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorDTO{" +
+                "username='" + username + '\'' +
+                ", avatar='" + avatar + '\'' +
+                '}';
+    }
+}
