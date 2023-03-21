be348cf3234660ab92b081df8a8ae624653e7b59
qasap <qa,sapaev@gmail.com>
2023-03-21 21:49:39 +0200

    DTO

--- sapaievProj/src/test/java/apiTests/apiTests.java
+++ sapaievProj/src/test/java/apiTests/apiTests.java
@@ -1,24 +1,71 @@
 package apiTests;
 
+import api.AuthorDTO;
 import api.Endpoints;
+import api.PostDTO;
 import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class apiTests {
     final String USER_NAME="autoapi";
+    private Logger logger=Logger.getLogger(getClass());
 
     @Test
     public void getPostsByUserTest(){
-    given()
+    PostDTO[] responseAsDTO=given()
             .contentType(ContentType.JSON)
             .log().all()
             .when()
             .get(Endpoints.POST_BY_USER,USER_NAME)
             .then()
             .statusCode(200)
-
+            .log().all()
+            .extract()
+            .response().as(PostDTO[].class)
     ;
+
+    logger.info("Number of Posts= " +responseAsDTO.length);
+    logger.info("Title post1 = "+responseAsDTO[0].getTitle());
+    logger.info("Username post1 = "+responseAsDTO[0].getAuthor().getUsername());
+
+    for (int i=0; i<responseAsDTO.length; i++){
+        Assert.assertEquals("Username is not matched" +i, USER_NAME, responseAsDTO[i].getAuthor().getUsername());
+    }
+
+    PostDTO[] expectedResult={
+            new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
+            new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
+    };
+
+    Assert.assertEquals("Number of posts ", expectedResult.length, responseAsDTO.length);
+
+        SoftAssertions softAssertions=new SoftAssertions();
+
+       for (int i=0; i<expectedResult.length; i++){
+            softAssertions.assertThat(responseAsDTO[i]).isEqualToIgnoringGivenFields(expectedResult[i],"id",
+                    "createdDate", "author");
+            softAssertions.assertThat(responseAsDTO[i].getAuthor()).isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(),"avatar");
+        }
+
+
+       /*
+       softAssertions.assertThat(responseAsDto[0])
+                        .usingRecursiveComparison()
+                        .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar", "isVisitorOwner")
+                        .isEqualTo(expectedResult[0]);
+        */
+
+
+        softAssertions.assertAll();
+
+
+
+
+
     }
 }
