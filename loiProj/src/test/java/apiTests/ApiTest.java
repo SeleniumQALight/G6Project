0f3a16db23008013ea6360bb1e529a6701a43d21
Loi_Emmanuel <emlzoloto@gmail.com>
2023-03-23 21:15:38 +0200

    JsonPath

--- loiProj/src/test/java/apiTests/ApiTest.java
+++ loiProj/src/test/java/apiTests/ApiTest.java
@@ -4,12 +4,15 @@ import api.AuthorDTO;
 import api.Endpoinds;
 import api.PostDTO;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.sql.Struct;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -79,5 +82,33 @@ public class ApiTest {
 
         Assert.assertEquals("Message in response ", "\"Sorry, invalid user requested.undefined\"", actualResponse);
         Assert.assertEquals("Message in response ", "Sorry, invalid user requested.undefined", actualResponse.replace("\"", ""));
+    }
+
+    @Test
+    public void getAllPostsByUserPath(){
+        Response actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(Endpoinds.POST_BY_USER, USER_NAME)
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response();
+
+        List<String> actualTitleList = actualResponse.jsonPath().getList("title", String.class);
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < actualTitleList.size(); i++) {
+            softAssertions.assertThat(actualTitleList.get(i)).as("item number " + i).contains("test");
+
+        }
+
+        List<Map> actualAuthorList = actualResponse.jsonPath().getList("author", Map.class);
+        for (int i = 0; i < actualAuthorList.size(); i++) {
+            softAssertions.assertThat(actualAuthorList.get(i).get("username")).as("item number " + i).isEqualTo(USER_NAME);
+        }
+
+        softAssertions.assertAll();
     }
 }
