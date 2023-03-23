2cacee1a44506cb1c4fca41319ef9c6ae7d0528b
smiian <smiyana.1989@gmail.com>
2023-03-23 20:14:11 +0200

    smiian-03-23 Lombok

--- smiianProj/src/test/java/apiTest/ApiTests.java
+++ smiianProj/src/test/java/apiTest/ApiTests.java
@@ -40,8 +40,14 @@ public class ApiTests {
         }
 
         PostDTO[] expectedResult = {
-                new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
-                new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
+//                new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,     //  були потрібні, коли не було анотацій
+//                new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
+                PostDTO.builder().title("test2").body("test body2").select1("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
+                        .build(),
+                PostDTO.builder().title("test").body("test body").select1("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
+                        .build()
         };
 
         Assert.assertEquals("Number of posts ", expectedResult.length, responseAsDTO.length);
