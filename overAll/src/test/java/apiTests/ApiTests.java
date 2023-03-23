40853067fff3eb91ad40073c436fa50f9c424357
Taras Radulenko <Taras_Radulenko@epam.com>
2023-03-23 21:42:37 +0200

    jsonSchema

--- overAll/src/test/java/apiTests/ApiTests.java
+++ overAll/src/test/java/apiTests/ApiTests.java
@@ -1,5 +1,7 @@
 package apiTests;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 import api.AuthorDTO;
 import api.EndPoints;
@@ -16,8 +18,9 @@ import org.junit.Test;
 public class ApiTests {
     final String USER_NAME = "autoapi";
     private Logger logger = Logger.getLogger(getClass());
+
     @Test
-    public void getPostsByUserTest(){
+    public void getPostsByUserTest() {
         PostDTO[] responseAsDto = given()
                 .contentType(ContentType.JSON)
                 .log().all()
@@ -27,21 +30,21 @@ public class ApiTests {
                 .statusCode(200)
                 .log().all()
                 .extract()
-                .response().as(PostDTO[].class)
-                ;
+                .response().as(PostDTO[].class);
 
         logger.info("Number of posts = " + responseAsDto.length);
-        logger.info("Title post1 = " + responseAsDto[0].getTitle()  );
+        logger.info("Title post1 = " + responseAsDto[0].getTitle());
         logger.info("Username post1 = " + responseAsDto[0].getAuthor().getUsername());
 
         for (int i = 0; i < responseAsDto.length; i++) {
             Assert.assertEquals("UserName is not matched in post " + i
-                    , USER_NAME , responseAsDto[i].getAuthor().getUsername());
+                    , USER_NAME, responseAsDto[i].getAuthor().getUsername());
         }
 
         PostDTO[] expectedResult = {
-//                new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
-//                new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
+                //                new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"),
+                //                false) ,
+                //                new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
                 PostDTO.builder().title("test2").body("test body2").select1("All Users").uniquePost("no")
                         .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
                         .build(),
@@ -54,17 +57,17 @@ public class ApiTests {
         Assert.assertEquals("Number of posts ", expectedResult.length, responseAsDto.length);
 
         SoftAssertions softAssertions = new SoftAssertions();
-//        for (int i = 0; i < expectedResult.length; i++) {
-//            softAssertions.assertThat(responseAsDto[i])
-//                    .isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
-//            softAssertions.assertThat(responseAsDto[i].getAuthor())
-//                    .isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
-//        }
+        //        for (int i = 0; i < expectedResult.length; i++) {
+        //            softAssertions.assertThat(responseAsDto[i])
+        //                    .isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
+        //            softAssertions.assertThat(responseAsDto[i].getAuthor())
+        //                    .isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
+        //        }
 
-                        softAssertions.assertThat(responseAsDto)
-                                .usingRecursiveComparison()
-                                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
-                                .isEqualTo(expectedResult);
+        softAssertions.assertThat(responseAsDto)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
+                .isEqualTo(expectedResult);
 
         softAssertions.assertAll();
 
@@ -73,7 +76,7 @@ public class ApiTests {
 
 
     @Test
-    public void getAllPostsByUserNegative(){
+    public void getAllPostsByUserNegative() {
         String actualResponse =
                 given()
                         .contentType(ContentType.JSON)
@@ -93,7 +96,7 @@ public class ApiTests {
     }
 
     @Test
-    public void getAllPostsByUserPath(){
+    public void getAllPostsByUserPath() {
         Response actualResponse =
                 given()
                         .contentType(ContentType.JSON)
@@ -120,10 +123,21 @@ public class ApiTests {
         softAssertions.assertAll();
 
 
-
-
     }
 
+    @Test
+    public void getAllPostsByUserSchema() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
+
+    }
 
 
 }
