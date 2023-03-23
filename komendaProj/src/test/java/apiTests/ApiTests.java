b2df325d1db71419de9c0ca19a323919fbfff576
komenda <wkomenda@gmail.com>
2023-03-23 19:26:00 +0200

    recursive

--- komendaProj/src/test/java/apiTests/ApiTests.java
+++ komendaProj/src/test/java/apiTests/ApiTests.java
@@ -46,15 +46,14 @@ public class ApiTests {
 
         SoftAssertions softAssertions = new SoftAssertions();
 
-        for (int i = 0; i < expectedResult.length; i++) {
-            softAssertions.assertThat(responseAsDto[i]).isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
-            softAssertions.assertThat(responseAsDto[i].getAuthor()).isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
-
-        }
-//        softAssertions.assertThat(responseAsDto[0])
-//            .usingRecursiveComparison()
-//                        .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar", "isVisitorOwner")
-//                        .isEqualTo(expectedResult[0]);
+//        for (int i = 0; i < expectedResult.length; i++) {
+//            softAssertions.assertThat(responseAsDto[i]).isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
+//            softAssertions.assertThat(responseAsDto[i].getAuthor()).isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
+//        }
+        softAssertions.assertThat(responseAsDto)
+             .usingRecursiveComparison()
+             .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
+             .isEqualTo(expectedResult);
 
         softAssertions.assertAll();
 
