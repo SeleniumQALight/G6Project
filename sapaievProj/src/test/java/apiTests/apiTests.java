6c1f6e8b8cccf8980e64419f87c4b89065935fc9
qasap <qa,sapaev@gmail.com>
2023-03-23 20:36:59 +0200

    response with String

--- sapaievProj/src/test/java/apiTests/apiTests.java
+++ sapaievProj/src/test/java/apiTests/apiTests.java
@@ -12,53 +12,52 @@ import org.junit.Test;
 import static io.restassured.RestAssured.given;
 
 public class apiTests {
-    final String USER_NAME="autoapi";
-    private Logger logger=Logger.getLogger(getClass());
+    final String USER_NAME = "autoapi";
+    private Logger logger = Logger.getLogger(getClass());
 
     @Test
-    public void getPostsByUserTest(){
-    PostDTO[] responseAsDTO=given()
-            .contentType(ContentType.JSON)
-            .log().all()
-            .when()
-            .get(Endpoints.POST_BY_USER,USER_NAME)
-            .then()
-            .statusCode(200)
-            .log().all()
-            .extract()
-            .response().as(PostDTO[].class)
-    ;
+    public void getPostsByUserTest() {
+        PostDTO[] responseAsDTO = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(Endpoints.POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response().as(PostDTO[].class);
 
-    logger.info("Number of Posts= " +responseAsDTO.length);
-    logger.info("Title post1 = "+responseAsDTO[0].getTitle());
-    logger.info("Username post1 = "+responseAsDTO[0].getAuthor().getUsername());
+        logger.info("Number of Posts= " + responseAsDTO.length);
+        logger.info("Title post1 = " + responseAsDTO[0].getTitle());
+        logger.info("Username post1 = " + responseAsDTO[0].getAuthor().getUsername());
 
-    for (int i=0; i<responseAsDTO.length; i++){
-        Assert.assertEquals("Username is not matched" +i, USER_NAME, responseAsDTO[i].getAuthor().getUsername());
-    }
+        for (int i = 0; i < responseAsDTO.length; i++) {
+            Assert.assertEquals("Username is not matched" + i, USER_NAME, responseAsDTO[i].getAuthor().getUsername());
+        }
 
-    PostDTO[] expectedResult={
-        //    new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
-        //    new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
-       //Пишем новый код с помощью lombok (аналогия закомментированного кода)
-       PostDTO.builder()
-               .title("test2").body("test body2").select1("All Users").uniquePost("no")
-               .author(AuthorDTO.builder().username("autoapi").build())
-               .isVisitorOwner(false)
-               .build(),
-            PostDTO.builder()
-                    .title("test").body("test body").select1("All Users").uniquePost("no")
-                    .author(AuthorDTO.builder().username("autoapi").build())
-                    .isVisitorOwner(false)
-                    .build()
-
-
-    };
-
-    Assert.assertEquals("Number of posts ", expectedResult.length, responseAsDTO.length);
+        PostDTO[] expectedResult = {
+                //    new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
+                //    new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
+                //Пишем новый код с помощью lombok (аналогия закомментированного кода)
+                PostDTO.builder()
+                        .title("test2").body("test body2").select1("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username("autoapi").build())
+                        .isVisitorOwner(false)
+                        .build(),
+                PostDTO.builder()
+                        .title("test").body("test body").select1("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username("autoapi").build())
+                        .isVisitorOwner(false)
+                        .build()
 
 
-    SoftAssertions softAssertions=new SoftAssertions();
+        };
+
+        Assert.assertEquals("Number of posts ", expectedResult.length, responseAsDTO.length);
+
+
+        SoftAssertions softAssertions = new SoftAssertions();
 
        /*for (int i=0; i<expectedResult.length; i++){
             softAssertions.assertThat(responseAsDTO[i]).isEqualToIgnoringGivenFields(expectedResult[i],"id",
@@ -67,19 +66,35 @@ public class apiTests {
         }*/
 
 
-       //аналогия закомментированного кода
-       softAssertions.assertThat(responseAsDTO)
-                        .usingRecursiveComparison()
-                        .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
-                        .isEqualTo(expectedResult);
-
+        //аналогия закомментированного кода
+        softAssertions.assertThat(responseAsDTO)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
+                .isEqualTo(expectedResult);
 
 
         softAssertions.assertAll();
 
+    }
 
 
-
+    @Test
+    public void getAllPostsByUserNegative() {
+        String actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(Endpoints.POST_BY_USER, "notValidUser")
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().getBody().asString();
+        Assert.assertEquals("Message in response ", "\"Sorry, invalid user requested.undefined\"", actualResponse);
+        Assert.assertEquals("Message in response ", "Sorry, invalid user requested.undefined", actualResponse
+                .replace("\"",""));
 
     }
+
+
 }
