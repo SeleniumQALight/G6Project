a1a633bc35cafe40bd19209bc910f4932872a844
EugenSydorenko <eugensydorenko@gmail.com>
2023-03-23 19:25:40 +0200

    recursive

--- sydorenkoProj/src/test/java/apiTest/ApiTest.java
+++ sydorenkoProj/src/test/java/apiTest/ApiTest.java
@@ -45,15 +45,15 @@ public class ApiTest {
 
         SoftAssertions softAssertions = new SoftAssertions();
 
-        for (int i = 0; i < expectedResult.length; i++) {
-            softAssertions.assertThat(responseAsDTO[i]).isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
-            softAssertions.assertThat(responseAsDTO[i].getAuthor()).isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(),"avatar");
-        }
+//        for (int i = 0; i < expectedResult.length; i++) {
+//            softAssertions.assertThat(responseAsDTO[i]).isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
+//            softAssertions.assertThat(responseAsDTO[i].getAuthor()).isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(),"avatar");
+//        }
 
-//        softAssertions.assertThat(responseAsDTO[0])
-//                .usingRecursiveComparison()
-//                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar", "isVisitorOwner")
-//                .isEqualTo(expectedResult[0]);
+        softAssertions.assertThat(responseAsDTO)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
+                .isEqualTo(expectedResult);
 
         softAssertions.assertAll();
     }
