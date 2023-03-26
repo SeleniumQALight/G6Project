069851d796d2c67e13f9b102c04d5d2b2bc87be0
yuliia soloviova <julysoloviova@gmail.com>
2023-03-26 17:49:29 -0300

    Response as a String.

--- soloviovaProj/src/test/java/apiTest/ApiTests.java
+++ soloviovaProj/src/test/java/apiTest/ApiTests.java
@@ -63,4 +63,21 @@ public class ApiTests {
                 .isEqualTo(expectedResult);
         softAssertions.assertAll();
     }
+
+    @Test
+    public void getAllPostsByUserNegative(){
+        String actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPoints.POST_BY_USER, "notValidUser")
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().getBody().asString();
+
+        Assert.assertEquals("Message in response ", "\"Sorry, invalid user requested.undefined\"", actualResponse);
+
+    }
 }
