dad6c3fe85b702e66f6a711ba60be2cabd4ad245
MarinaShtanko <astarohsi14@gmail.com>
2023-03-27 20:04:48 +0300

    dto_new

--- shtankoProj/src/test/java/apiTests/ApiTests.java
+++ shtankoProj/src/test/java/apiTests/ApiTests.java
@@ -4,12 +4,17 @@ import api.AuthorDTO;
 import api.EndPoints;
 import api.PostDTO;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 
 public class ApiTests {
@@ -36,25 +41,87 @@ public class ApiTests {
                     USER_NAME, responseAsDTO[i].getAuthor().getUsername());
         }
         PostDTO[] expectedResult = {
-                new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
-                new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
+                PostDTO.builder().title("test2").body("test body2").select1("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
+                        .build(),
+                PostDTO.builder().title("test").body("test body").select1("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
+                        .build()
+//                new PostDTO("test2","test body2", "All Users", "no", new AuthorDTO("autoapi"), false) ,
+//                new PostDTO("test","test body","All Users","no", new AuthorDTO("autoapi"), false)
         };
 
         Assert.assertEquals("Number of post", expectedResult.length, responseAsDTO.length);
         SoftAssertions softAssertions = new SoftAssertions();
-        for (int i = 0; i < expectedResult.length; i++) {
-            softAssertions.assertThat(responseAsDTO[i])
-                    .isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
-            softAssertions.assertThat(responseAsDTO[i].getAuthor())
-                    .isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
-        }
-//        softAssertions.assertThat(responseAsDTO[0])
-//                .usingRecursiveComparison()
-//                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar", "isVisitorOwner")
-//                .isEqualTo(expectedResult[0]);
+//        for (int i = 0; i < expectedResult.length; i++) {
+//            softAssertions.assertThat(responseAsDTO[i])
+//                    .isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
+//            softAssertions.assertThat(responseAsDTO[i].getAuthor())
+//                    .isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
+//        }
+        softAssertions.assertThat(responseAsDTO)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate", "isVisitorOwner", "author.avatar")
+                .isEqualTo(expectedResult);
 
         softAssertions.assertAll();
 
+
+    }
+
+    @Test
+    public void getAllPostsByUserNegative(){
+        String actualResponse = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POST_BY_USER, "notValidUser")
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response().getBody().asString();
+
+        Assert.assertEquals("Message in response", "\"Sorry, invalid user requested.undefined\"", actualResponse);
+    }
+
+    @Test
+    public void getAllPostsByUserPath(){
+        Response actualResponse = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response();
+        List<String> actualTitleList = actualResponse.jsonPath().getList("title", String.class);
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i < actualTitleList.size(); i++) {
+            softAssertions.assertThat(actualTitleList.get(i)).as("Item number " + i).contains("test");
+        }
+        List<Map> actualAuthorList = actualResponse.jsonPath().getList("author", Map.class);
+        for (int i = 0; i < actualTitleList.size(); i++) {
+            softAssertions.assertThat(actualAuthorList.get(i).get("username"))
+                    .as("Item number " + i).isEqualTo(USER_NAME);
+
+        }
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void getAllPostsByUserSchema(){
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POST_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
 
     }
 }
