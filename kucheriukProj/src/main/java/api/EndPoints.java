1732719b226e2eb588cccedff7ba19020db1696c
ludmilakucheriuk <lusiya.dep@gmail.com>
2023-03-28 22:01:23 +0300

    Created Post by Api test

--- kucheriukProj/src/main/java/api/EndPoints.java
+++ kucheriukProj/src/main/java/api/EndPoints.java
@@ -5,4 +5,7 @@ public interface EndPoints {
     String POST_BY_USER = baseUrl + "/api/postsByAuthor/{0}";
 
     String PB_EXCHANGE_RATES_URL = "https://api.privatbank.ua/p24api/exchange_rates";
+    String LOGIN = baseUrl + "/api/login";
+    String CREATE_POST = baseUrl + "/api/create-post";
+    String DELETE_POST = baseUrl + "/api/post/{0}";
 }
