ba9a97e8793511143dd71b1fb035f0b730f6d3c6
qasap <qa,sapaev@gmail.com>
2023-03-29 17:17:08 +0300

    API. Home work

--- sapaievProj/src/test/java/apiTests/apiTestPrivat.java
+++ sapaievProj/src/test/java/apiTests/apiTestPrivat.java
@@ -1,10 +1,9 @@
 package apiTests;
 
-import api.Endpoints;
-import api.EndpointsPrivatBank;
-import api.PostDTO;
-import api.PrivatBankDTO;
+import api.*;
 import io.restassured.http.ContentType;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,21 +11,113 @@ import static io.restassured.RestAssured.given;
 public class apiTestPrivat {
 
 
-
     @Test
     public void getPostsByUserTest() {
-         given()
-                .contentType(ContentType.JSON).queryParam("date","22.03.2022")
+        PrivatBankDTO responseObjectDTO = given()
+                .contentType(ContentType.JSON).queryParam("date", "22.03.2022")
                 .log().all()
                 .when()
                 .get(EndpointsPrivatBank.requestPrivat)
                 .then()
                 .statusCode(200)
                 .log().all()
-                 .extract()
-                 .response().as(PrivatBankDTO.class)
+                .extract()
+                .response().as(PrivatBankDTO.class);
 
-         ;
+        Assert.assertTrue("", responseObjectDTO.getExchangeRate().length == 25);
+
+        PrivatBankDTO expectedResult = new PrivatBankDTO("PB", "UAH",
+
+                new ExchangeRateDTO[]{
+                        new
+                                ExchangeRateDTO("UAH", "AUD"),
+                        new
+
+                                ExchangeRateDTO("UAH", "AZN"),
+                        new
+
+                                ExchangeRateDTO("UAH", "BYN"),
+                        new
+
+                                ExchangeRateDTO("UAH", "CAD"),
+                        new
+
+                                ExchangeRateDTO("UAH", "CHF"),
+                        new
+
+                                ExchangeRateDTO("UAH", "CNY"),
+                        new
+
+                                ExchangeRateDTO("UAH", "CZK"),
+                        new
+
+                                ExchangeRateDTO("UAH", "DKK"),
+                        new
+
+                                ExchangeRateDTO("UAH", "EUR"),
+                        new
+
+                                ExchangeRateDTO("UAH", "GBP"),
+                        new
+
+                                ExchangeRateDTO("UAH", "GEL"),
+                        new
+
+                                ExchangeRateDTO("UAH", "HUF"),
+
+                        new
+
+                                ExchangeRateDTO("UAH", "ILS"),
+                        new
+
+                                ExchangeRateDTO("UAH", "JPY"),
+                        new
+
+                                ExchangeRateDTO("UAH", "KZT"),
+                        new
+
+                                ExchangeRateDTO("UAH", "MDL"),
+                        new
+
+                                ExchangeRateDTO("UAH", "NOK"),
+                        new
+
+                                ExchangeRateDTO("UAH", "PLN"),
+                        new
+
+                                ExchangeRateDTO("UAH", "SEK"),
+                        new
+
+                                ExchangeRateDTO("UAH", "SGD"),
+                        new
+
+                                ExchangeRateDTO("UAH", "TMT"),
+                        new
+
+                                ExchangeRateDTO("UAH", "TRY"),
+                        new
+
+                                ExchangeRateDTO("UAH", "UAH"),
+                        new
+
+                                ExchangeRateDTO("UAH", "USD"),
+                        new
+
+                                ExchangeRateDTO("UAH", "UZS")
+
+                });
+
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(responseObjectDTO)
+                .usingRecursiveComparison()
+                .ignoringFields(
+                        "date", "baseCurrency",
+                        "exchangeRate.saleRateNB", "exchangeRate.purchaseRateNB", "exchangeRate.saleRate", "exchangeRate.purchaseRate")
+                .isEqualTo(expectedResult);
+
+
+        softAssertions.assertAll();
 
     }
 }
