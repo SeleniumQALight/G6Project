a4b513c6f743df4efdef925f57d43f6c5b9e3fbb
smiian <smiyana.1989@gmail.com>
2023-03-30 22:36:29 +0300

    smiian-03-30 some API + cucumber

--- smiianProj/src/test/java/suits/SmokeSuite.java
+++ smiianProj/src/test/java/suits/SmokeSuite.java
@@ -11,8 +11,8 @@ import postTest.CreatePostTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LoginTestWithPageObject.class,
-        CreatePostTest.class
-//        ApiTests.class
+        CreatePostTest.class,
+        ApiTests.class
 })
 public class SmokeSuite {
 }
