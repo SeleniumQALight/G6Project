9ead20502e839ece44546c326434bf6b3ac03ded
komenda <wkomenda@gmail.com>
2023-03-30 15:45:58 +0300

    check the fields + create a schema + check exchange rates greater than 0

--- komendaProj/src/main/java/api/EndPoints.java
+++ komendaProj/src/main/java/api/EndPoints.java
@@ -3,4 +3,14 @@ package api;
 public interface EndPoints {
      String baseUrl = "https://qa-complexapp.onrender.com";
      String POST_BY_USER = baseUrl + "/api/postsByAuthor/{0}";
+
+
+
+
+
+
+
+     String baseUrlPB = "https://api.privatbank.ua";
+     String EXCHANGE_RATES_PB = baseUrlPB + "/p24api/exchange_rates?date={0}";
+     String EXCHANGE_RATES_PB1 = baseUrlPB + "/p24api/exchange_rates";
 }
