00a6156dc685792547bbe8c722644a4b4254cd6b
Nataliia Golenko <nataliepinchuk@gmail.com>
2023-04-04 18:56:05 +0300

    HW2 - part3

--- golenkoProj/src/test/java/apiTests/ApiTestBooks.java
+++ golenkoProj/src/test/java/apiTests/ApiTestBooks.java
@@ -1,23 +1,29 @@
 package apiTests;
 
 import api.ApiHelperBooks;
-import api.EndPoints;
 import api.EndPointsBooks;
+import api.dto.books.AddBookDTO;
+import api.dto.books.BooksListDTO;
+import api.dto.books.IsbnDTO;
 import io.restassured.http.ContentType;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
 public class ApiTestBooks {
     ApiHelperBooks apiHelperBooks = new ApiHelperBooks();
     Logger logger = Logger.getLogger(getClass());
+    String token = apiHelperBooks.getToken();
+    String userID = apiHelperBooks.getUserID();
 
     @Before
-    public void deleteBooksForUser(){
-        String token = apiHelperBooks.getToken();
-        String userID = apiHelperBooks.getUserID();
+    public void deleteBooksForUser() {
+
         given()
                 .contentType(ContentType.JSON)
                 .log().all()
@@ -31,10 +37,34 @@ public class ApiTestBooks {
 
 
     @Test
-    public void checkBookStore(){
-        String token = apiHelperBooks.getToken();
-        String userID = apiHelperBooks.getUserID();
+    public void checkBookAdding() {
+        BooksListDTO listOfAllBooks =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPointsBooks.LIST_OF_ALL_BOOKS)
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().as(BooksListDTO.class);
 
+        String isbn = listOfAllBooks.getBooks().get(0).getIsbn();
+
+        List<IsbnDTO> collectionIsbn = new ArrayList<>();
+        collectionIsbn.add(new IsbnDTO(isbn));
+
+        AddBookDTO addOneBook = AddBookDTO.builder()
+                .userId(userID)
+                .collectionOfIsbns(collectionIsbn)
+                .build();
+
+
+//        String response =
+//                given()
+//                        .contentType(ContentType.JSON)
+//                        .log().all()
+//                        .when()
 
 
     }
