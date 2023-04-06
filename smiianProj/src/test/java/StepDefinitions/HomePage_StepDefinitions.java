f88861cbc2e85a919b371322f2670e26aa0bc78a
smiian <smiyana.1989@gmail.com>
2023-04-06 22:02:00 +0300

    smiian-04-06-API

---
+++ smiianProj/src/test/java/StepDefinitions/HomePage_StepDefinitions.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import libs.DriverHelper;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class HomePage_StepDefinitions {
+//    LoginPage loginPage = new LoginPage(DriverHelper.getWebDriver());
+    HomePage homePage = new HomePage(DriverHelper.getWebDriver());
+
+
+    @Given("^User open 'Home' page$")
+    public void userOpenHomePage() {
+        homePage.openHomePage();
+    }
+
+    @When("^User opens 'MyProfile' button on 'Home' page$")
+    public void userClicksOnMyProfileButtonOnHomePage() {
+        homePage.getHeaderElement().clickOnMyProfileButton();
+    }
+}
