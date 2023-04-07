4a3a04e3d4aa358595e234361869f7f69e069c8b
komenda <wkomenda@gmail.com>
2023-04-07 19:00:44 +0300

    HW#2

---
+++ komendaProj/src/main/java/api/dto/books/BooksDTO.java
@@ -0,0 +1,21 @@
+package api.dto.books;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BooksDTO {
+    String isbn;
+    String title;
+    String subTitle;
+    String author;
+    String publish_date;
+    String publisher;
+    Integer pages;
+    String description;
+    String website;
+}
