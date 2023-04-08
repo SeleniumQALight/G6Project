10e780dfa29ada8d4720d721eaf4012ea60989a0
MarinaShtanko <astarohsi14@gmail.com>
2023-04-08 23:07:21 +0300

    hw api test privat #1

---
+++ shtankoProj/src/main/java/api/EndPointsPrivatBank.java
@@ -0,0 +1,6 @@
+package api;
+
+public interface EndPointsPrivatBank {
+    String baseUrlPrivat = "https://api.privatbank.ua";
+    String EXCHANGE_RATES = baseUrlPrivat + "/p24api/exchange_rates";
+}
