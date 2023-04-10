483783e2721fa8c4da9c0e7a14875b6ebb6e3221
volodymyrchyrva <volodymyr.chyrva@yahoo.com>
2023-04-10 10:05:53 -0300

    Feature,Full Regression

--- chyrvaProj/src/main/java/api/ApiHelper.java
+++ chyrvaProj/src/main/java/api/ApiHelper.java
@@ -1,5 +1,6 @@
 package api;
 
+import api.dto.requestDto.CreatePostDTO;
 import api.dto.responseDto.PostDTO;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
@@ -69,7 +70,7 @@ RequestSpecification requestSpecification = new RequestSpecBuilder()
 
     }
 
-    private void deletePostsTillPresent(String userName, String password) {
+    public void deletePostsTillPresent(String userName, String password) {
         PostDTO[] listOfPosts = getAllPostsByUser(userName);
         String token = getToken(userName,password);
 
@@ -102,4 +103,28 @@ RequestSpecification requestSpecification = new RequestSpecBuilder()
 
     }
 
+    public void createPost(String userName, String password, String title) {
+        String token = getToken(userName.toLowerCase(),password);
+
+        CreatePostDTO createPostDTO = CreatePostDTO.builder()
+                .title(title)
+                .body("Post Body")
+                .select1("One Person")
+                .uniquePost("yes")
+                .token(token)
+                .build();
+
+        String response = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(createPostDTO)
+                .when()
+                .post(EndPoints.CREATE_POST)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response().getBody().asString();
+
+        Assert.assertEquals("Message","\"Congrats.\"",response);
+    }
 }
