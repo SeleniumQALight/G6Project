64f58fb77387f69d71b3b438ee77c1b7d77363fa
IvanYakymiv <yakymiv.ivan@gmail.com>
2023-04-11 16:30:08 +0200

    Fixing conflict

--- yakymivProj/src/test/java/StepDefinitions/Homepage_StepDefinitions.java
+++ yakymivProj/src/test/java/StepDefinitions/Homepage_StepDefinitions.java
@@ -1,11 +1,12 @@
 package StepDefinitions;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import libs.DriverHelper;
 import pages.HomePage;
 
-public class HomePage_StepDefinitions {
+public class Homepage_StepDefinitions {
     HomePage homePage = new HomePage(DriverHelper.getWebDriver());
 
     @Given("^User opens 'Home' page$")
@@ -16,5 +17,10 @@ public class HomePage_StepDefinitions {
     @When("^User click on 'MyProfile' button on 'Home' page$")
     public void userClickOnMyProfileButtonOnHomePage() {
         homePage.getHeaderElement().clickOnMyProfileButton();
+    }
+
+    @Then("^User sees Sign Out button$")
+    public void user_sees_Sign_Out_button() throws Throwable {
+        homePage.checkIsRedirectHomePage();
     }
 }
