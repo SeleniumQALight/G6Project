6798b9d4944da70e9a0e5dc04093c37236355437
smiian <smiyana.1989@gmail.com>
2023-04-16 22:38:13 +0300

    smiian-API-Project

---
+++ smiianProj/src/main/java/api/EndPointsProj.java
@@ -0,0 +1,12 @@
+package api;
+
+public interface EndPointsProj {
+    String baseUrl = "https://api.privatbank.ua";
+    String GET_USER = baseUrl + "/p24api/pubinfo";
+
+    //https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5
+
+//    String LOGIN = baseUrl + "/api/login";
+//    String CREATE_POST = baseUrl + "/api/create-post";
+//    String DELETE_POST = baseUrl + "/api/post/{0}";
+}
